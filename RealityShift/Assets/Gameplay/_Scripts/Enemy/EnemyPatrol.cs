3c0e622f20f615a03e47a86404c838fa73481760
Pr0gr1m <83018483+Pr0gr1m@users.noreply.github.com>
2022-05-20 21:39:16 +0200

    Fix of something

--- RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyPatrol.cs
+++ RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyPatrol.cs
@@ -7,10 +7,9 @@ public class EnemyPatrol : MonoBehaviour
 {
     [SerializeField] private List<Vector2> walkPoints = new List<Vector2>();
     [SerializeField] private bool move = true;
-<<<<<<< HEAD:RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyMovement.cs
-=======
+
     bool theGameIsRunning;
->>>>>>> 0a9fd7c14fe140e9d9c0ded6b399d1eceda95110:RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyPatrol.cs
+
     int currentWalkPoint;
     Vector2 initialPos;
     Rigidbody2D rig;
@@ -39,14 +38,6 @@ public class EnemyPatrol : MonoBehaviour
             currentWalkPoint++;
             if (currentWalkPoint == walkPoints.Count) { currentWalkPoint = 0; }
         }
-    }
-
-    public void OnColliderEnter2D(Collider other)
-    {
-        if(!(other.gameObject.tag == "Player")) return;
-
-        PlayerController c = other.gameObject.GetComponent<PlayerController>();
-        c.AddHealth(Damage);
     }
 
     private void OnDrawGizmosSelected()
