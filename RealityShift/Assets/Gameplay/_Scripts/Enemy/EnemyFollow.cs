2dbf17fb7a4f7577656229f71163bf4c6e0a4a52
IgorBorgesDaMata <igorbdamata@gmail.com>
2022-05-20 16:25:17 -0300

    Enemy follow
    I restructured the enemy codes, and now the enemy has a movementCotroller, patrol and follow scripts. The first its a global code for movements, has method to follow object, speed variable, etc. The second its the code who do enemy move, and the third do enemy follow the player when he its on the vision radius, it has a three variables: visionRadius (Vector2), minTimeToStop and maxTimeToStop (floats). The timeToStop variables are used to enemy has a delay to stop follow when player exit of her visionRadius (idea of Maki), that delay its a random number between min and max time to stop

---
+++ RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyFollow.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    [SerializeField] private Vector2 visionRadius;
+    [SerializeField] [Range(1, 10)] private float minTimeToStopFollow;
+    [SerializeField] [Range(1, 10)] private float maxTimeToStopFollow;
+    Transform player;
+    EnemyMovementController movement;
+    public bool isFollowing { get; private set; }
+    bool isStoppingFollow;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>().transform;
+        movement = GetComponent<EnemyMovementController>();
+    }
+
+    private void Update()
+    {
+        if (PlayerInVisionRadius()) { isFollowing = true; }
+        else if (isFollowing) { StartCoroutine(StopFollow()); }
+
+        if (isFollowing)
+        {
+            movement.FollowObject(player.position);
+        }
+    }
+
+    IEnumerator StopFollow()
+    {
+        if (!isStoppingFollow)
+        {
+            isStoppingFollow = true;
+            yield return new WaitForSeconds(Random.Range(minTimeToStopFollow, maxTimeToStopFollow));
+            isStoppingFollow = false;
+            isFollowing = false;
+        }
+    }
+
+    bool PlayerInVisionRadius()
+    {
+        Vector2 distance = player.position - transform.position;
+        float rotation = transform.localEulerAngles.y;
+        bool enemyIsSeeingThePlayer = false;
+        if (Mathf.Abs(distance.x) <= visionRadius.x && Mathf.Abs(distance.y) <= visionRadius.y)
+        {
+            enemyIsSeeingThePlayer = distance.x > 0 && rotation == 0 || distance.x < 0 && rotation == 180;
+        }
+
+        return enemyIsSeeingThePlayer;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        
+        Gizmos.color = Color.red;
+        float endPosX = transform.position.x + visionRadius.x * (transform.localEulerAngles.y == 180 ? -1 : 1);
+        Gizmos.DrawLine(transform.position, new Vector3(endPosX, transform.position.y));
+        Gizmos.DrawLine(new Vector3(endPosX, transform.position.y), new Vector3(endPosX, transform.position.y + visionRadius.y));
+    }
+}
--- RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyPatrol.cs
+++ RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyPatrol.cs
@@ -1,36 +1,36 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(EnemyMovementController))]
 [RequireComponent(typeof(Rigidbody2D))]
-public class EnemyMovement : MonoBehaviour
+public class EnemyPatrol : MonoBehaviour
 {
     [SerializeField] private List<Vector2> walkPoints = new List<Vector2>();
-    [SerializeField] private float speed;
     [SerializeField] private bool move = true;
+    bool theGameIsRunning;
     int currentWalkPoint;
     Vector2 initialPos;
     Rigidbody2D rig;
-    bool theGameIsRunning;
+    EnemyMovementController movement;
 
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        movement = GetComponent<EnemyMovementController>();
         initialPos = transform.position;
         theGameIsRunning = true;
     }
 
     private void Update()
     {
-        if (move) { Move(); }         
+        if (move) { Move(); }
     }
 
     public void Move()
     {
-        Vector2 direction = new Vector2(Mathf.Clamp(walkPoints[currentWalkPoint].x, -1, 1), 0);
-        rig.velocity = new Vector2(direction.x * speed * Time.deltaTime, rig.velocity.y);
-        float finalPlace = walkPoints[currentWalkPoint].x + initialPos.x;
-        bool isReached = Mathf.Abs(transform.position.x - finalPlace) <= 0.1f;
+        Vector2 finalPlace = walkPoints[currentWalkPoint] + initialPos;
+        movement.FollowObject(finalPlace);
+        bool isReached = Mathf.Abs(transform.position.x - finalPlace.x) <= 0.1f;
         if (isReached)
         {
             currentWalkPoint++;
