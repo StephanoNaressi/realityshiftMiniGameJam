950bf883144c9faca10f225733697d0e1556abaf
IgorBorgesDaMata <igorbdamata@gmail.com>
2022-05-20 17:42:48 -0300

    Reupload

---
+++ RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyMovementController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyMovementController : MonoBehaviour
+{
+    [SerializeField] private float walkingSpeed = 100;
+    [SerializeField] private float runningSpeed = 150;
+    public float Damage;
+    private float speed;
+    Rigidbody2D rig;
+
+    private void Start()
+    {
+        speed = walkingSpeed;
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        ChangeSeeDirection();
+        if (GetComponent<EnemyFollow>() && GetComponent<EnemyPatrol>())
+        {
+            if (!GetComponent<EnemyFollow>().isFollowing)
+            {
+                speed = walkingSpeed;
+                GetComponent<EnemyPatrol>().Move();
+            }
+            else
+            {
+                speed = runningSpeed;
+            }
+        }
+    }
+
+    void ChangeSeeDirection()
+    {
+        if (rig.velocity.x != 0)
+        {
+            transform.localEulerAngles = new Vector3(0, rig.velocity.x > 0 ? 0 : 180, 0);
+        }
+    }
+
+    public void OnColliderEnter2D(Collider other)
+    {
+        if (!(other.gameObject.tag == "Player")) return;
+
+        PlayerController c = other.gameObject.GetComponent<PlayerController>();
+        c.AddHealth(Damage);
+    }
+
+    public void FollowObject(Vector2 objectPos)
+    {
+        float direction = 0;
+        if (transform.position.x > objectPos.x)
+        {
+            direction = -speed;
+        }
+        else if (transform.position.x < objectPos.x)
+        {
+            direction = speed;
+        }
+        rig.velocity = new Vector2(direction * Time.deltaTime, rig.velocity.y);
+    }
+}
