6f2906e1cc836b4c49be1f7f9103c2ccb252a450
IgorBorgesDaMata <igorbdamata@gmail.com>
2022-05-20 14:52:10 -0300

    Enemy movement
    Her variables are walkPoints (list of Vector2), speed (float) and move (bool). The walkPoints is a places where the enemy will go. The boolean move it's true only if the enemy needs to be always walking, if he has other behaviours, this boolean need to be false, and to enemy walk, other code need to call Move() method. I added a example of enemy in SampleScene. Any doubt feel free to ask me

---
+++ RealityShift/Assets/Gameplay/_Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private List<Vector2> walkPoints = new List<Vector2>();
+    [SerializeField] private float speed;
+    [SerializeField] private bool move = true;
+    int currentWalkPoint;
+    Vector2 initialPos;
+    Rigidbody2D rig;
+    bool theGameIsRunning;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        initialPos = transform.position;
+        theGameIsRunning = true;
+    }
+
+    private void Update()
+    {
+        if (move) { Move(); }         
+    }
+
+    public void Move()
+    {
+        Vector2 direction = new Vector2(Mathf.Clamp(walkPoints[currentWalkPoint].x, -1, 1), 0);
+        rig.velocity = new Vector2(direction.x * speed * Time.deltaTime, rig.velocity.y);
+        float finalPlace = walkPoints[currentWalkPoint].x + initialPos.x;
+        bool isReached = Mathf.Abs(transform.position.x - finalPlace) <= 0.1f;
+        if (isReached)
+        {
+            currentWalkPoint++;
+            if (currentWalkPoint == walkPoints.Count) { currentWalkPoint = 0; }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!theGameIsRunning) { initialPos = transform.position; }
+
+        Gizmos.color = Color.green;
+        foreach (Vector2 walkP in walkPoints)
+        {
+            Gizmos.DrawSphere(initialPos + walkP, 0.1f);
+        }
+    }
+}
