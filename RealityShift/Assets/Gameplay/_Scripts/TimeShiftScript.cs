6c573d0cf03cdcae5a9308abc73875af4e010ee0
Stephano Naressi <nanbatsu@outlook.com>
2022-05-20 16:01:38 +0100

    Added comments

--- RealityShift/Assets/Gameplay/_Scripts/TimeShiftScript.cs
+++ RealityShift/Assets/Gameplay/_Scripts/TimeShiftScript.cs
@@ -18,13 +18,17 @@ public class TimeShiftScript : MonoBehaviour
     {
         if (isShifted)
         {
+            //Reality has been shifted
             shaderM.SetFloat("_IsShift", 0f);
+            //Set reality to normal
             isShifted = false;
         }
         else
         {
+            //Break the reality
             shaderM.SetFloat("_IsShift", 1f);
             isShifted = true;
+            //Add reality logic here!
         }
         
     }
