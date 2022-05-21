50a4f80bdc69323ca15c8ab7ddcac4382e8d4ade
Stephano Naressi <nanbatsu@outlook.com>
2022-05-21 14:06:47 +0100

    Added Dithering when reality shifting, plus timer script

--- RealityShift/Assets/Gameplay/_Scripts/TimeShiftScript.cs
+++ RealityShift/Assets/Gameplay/_Scripts/TimeShiftScript.cs
@@ -9,12 +9,55 @@ public class TimeShiftScript : MonoBehaviour
     bool isShifted = false;
     [SerializeField]
     GameObject hiddenLayer;
+
+    int ShiftCount;
+    [SerializeField]
+    float timer;
+    float startingTimer;
+    float shiftCount;
+    [SerializeField]
+    TrailRenderer trail;
+    private void Start()
+    {
+        trail.enabled = false;
+        shiftCount = 0f;
+        startingTimer = timer;
+        isShifted = false;
+        shaderM.SetFloat("_IsShift", 0f);
+        shaderM.SetFloat("_TimesShifted", 0f);
+    }
     private void Update()
     {
         if (Input.GetButtonDown("realityShift"))
         {
             TimeShift();
+            
         }
+        if(isShifted == true)
+        {
+            timerF();
+        }
+
+        
+    }
+    bool timerF()
+    {
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            
+        }
+        else
+        {
+            TimeShift();
+            timer = startingTimer;
+            shiftCount+= 0.2f;
+            shaderM.SetFloat("_TimesShifted", shiftCount);
+            return false;
+        }
+        return true;
+        
     }
     void TimeShift()
     {
@@ -23,6 +66,7 @@ public class TimeShiftScript : MonoBehaviour
             //Reality has been shifted
             shaderM.SetFloat("_IsShift", 0f);
             //Set reality to normal
+            trail.enabled = false;
             isShifted = false;
             hiddenLayer.SetActive(false);
         }
@@ -30,8 +74,10 @@ public class TimeShiftScript : MonoBehaviour
         {
             //Break the reality
             shaderM.SetFloat("_IsShift", 1f);
-            isShifted = true;
+            
+            trail.enabled = true;
             //Add reality logic here!
+            isShifted = true;
             hiddenLayer.SetActive(true);
         }
         
